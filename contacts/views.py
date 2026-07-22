d3a48115b2ba56d7d36ac16600d91440b69943a0
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-22 15:46:09 +0530

    added views.py

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Contact
+from .serializers import ContactSerializer
 
-# Create your views here.
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    filterset_fields = ["comapny"]
+    search_fields = ["name", "email", "phone"]
+    ordering_fields = "__all__"
