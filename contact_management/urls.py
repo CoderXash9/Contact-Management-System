8a04f339e584ff36495d54a5e96078f542f1dcb5
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-23 15:05:47 +0530

    added urls.py in contact_managaemnt

--- contact_management/urls.py
+++ contact_management/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path  # allows one url file to use another url file
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "api/", include("contacts.urls")
+    ),  # if URL starts with /api/ , go to contacts/urls/py
 ]
