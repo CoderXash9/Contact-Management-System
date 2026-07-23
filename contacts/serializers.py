ef57ac05d7170c17815d7880a799d41019424ea1
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-24 00:07:26 +0530

    validating phone number

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -6,3 +6,14 @@ class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
         fields = "__all__"
+
+    def validate_phone(self, value):
+        if not value.isdigit():
+            raise serializers.ValidationError(
+                "Phone number must contain only 10 digits."
+            )
+
+        if len(value) != 10:
+            raise serializers.ValidationError("Phone number must be exactly 10 digits.")
+
+        return value
